01ad64851bb20044d31435c85f5710e6d590c833
DP <mint.dropout-3d@icloud.com>
2025-10-30 14:09:45 +0100

    Update lib.rs

--- style/lib.rs
+++ style/lib.rs
@@ -1,3 +1,6 @@
+#![recursion_limit = "1024"]
+// Optional: only if you also hit E0272 "type length limit exceeded"
+#![type_length_limit = "2000000"]
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
